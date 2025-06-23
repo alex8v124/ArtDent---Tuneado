348335f71533d93da1c205393db85501dea0897c
Juan Alexander Miranda <alex.04.antay@gmail.com>
2025-06-23 02:17:15 +0000

    reduce un poco la parte de arriba y aumenta un poco la parte de abajo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function HomePage() {
         </div>
       </section>
 
-      <section className="container mx-auto px-2 py-32">
+      <section className="container mx-auto px-2 pt-24 pb-40">
         <h2 className="text-3xl font-headline font-bold text-center mb-10 text-primary">Explore Nuestras Secciones</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <Link href="/servicios" className="block group">
