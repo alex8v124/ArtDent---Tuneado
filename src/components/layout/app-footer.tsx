b4d7092291449435f9d743e281d3cb92c8982a0c
Juan Alexander Miranda <alex.04.antay@gmail.com>
2025-06-23 01:24:15 +0000

    elimina el icono de youtube sonde dice siguenos porfis

--- src/components/layout/app-footer.tsx
+++ src/components/layout/app-footer.tsx
@@ -16,9 +16,6 @@ const FacebookIcon = (props: React.SVGProps<SVGSVGElement>) => (
 const InstagramIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>
 );
-const YoutubeIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2.5 17a24.12 24.12 0 0 1 0-10C2.5 6 7.5 4 12 4s9.5 2 9.5 3-2.5 2.7-2.5 2.7S21.5 12 21.5 13a24.12 24.12 0 0 1 0 10c0 1-5 3-9.5 3s-9.5-2-9.5-3z"></path><path d="m10 15 5-3-5-3z"></path></svg>
-);
 
 
 const VisaIcon = (props: React.SVGProps<SVGSVGElement>) => (
@@ -146,7 +143,6 @@ const AppFooter = () => {
                         <div className="flex space-x-4">
                             <a href="https://www.instagram.com/artdent._/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="hover:text-white transition-colors"><InstagramIcon /></a>
                             <a href="https://www.facebook.com/ARTDENT000" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="hover:text-white transition-colors"><FacebookIcon /></a>
-                            <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Youtube" className="hover:text-white transition-colors"><YoutubeIcon /></a>
                         </div>
                     </div>
                 </div>
@@ -231,3 +227,5 @@ const AppFooter = () => {
 };
 
 export default AppFooter;
+
+    
